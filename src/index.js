485eb57f3f185218c6eb911d7794beb86b338d5a
Reventiss <117314942+Reventiss@users.noreply.github.com>
2023-11-13 20:18:09 +0000

    zmiana index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register('sw.js')
-    .then((register) => console.log('SerwiceWorker zarejestrowany', register))
-    .catch((error) => console.log('Rejestracja SerwiceWorker nie powiodła się!', error));
+const checkPermission = () => {
+    if (!('serviceWorker' in navigator)) {
+        throw new ErrorEvent("Mo support for service worker!")
+    }
+    
+    if (!('Notification' in window)) {
+        throw new ErrorEvent("No support notification API!");
+    }
 }
+
+const registerSW = async () => {
+    const registration = await navigator.serviceWorker.register('sw.js');
+    return registration;
+}
+
+const requestNotificationPermission = async () => {
+    const permission = await Notification.requestPermission();
+
+    if (permission !== 'granted') {
+        throw new Error("Notification permission not granted")
+    } else {
+        new Notification("Hello world");
+    }
+}
+
+checkPermission()
+registerSW()
+requestNotificationPermission()

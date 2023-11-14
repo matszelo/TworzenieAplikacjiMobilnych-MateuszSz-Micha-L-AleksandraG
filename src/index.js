545f531e462c49cad539a5784448d672e631dc1c
Reventiss <117314942+Reventiss@users.noreply.github.com>
2023-11-14 21:19:15 +0000

    powazne zmiany

--- src/index.js
+++ src/index.js
@@ -13,24 +13,46 @@ const registerSW = async () => {
     return registration;
 }
 
-const requestNotificationPermission = async () => {
-    const permission = await Notification.requestPermission();
 
-    if (permission !== 'granted') {
-        throw new Error("Notification permission not granted")
-    } else {
-        new Notification("Hello world");
+
+function geoFindMe() {
+    const status = document.querySelector("#status");
+    const mapLink = document.querySelector("#map-link");
+  
+    mapLink.href = "";
+    mapLink.textContent = "";
+  
+    function success(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = "";
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
     }
-}
+  
+    function error() {
+      status.textContent = "Unable to retrieve your location";
+    }
+  
+    if (!navigator.geolocation) {
+      status.textContent = "Geolocation is not supported by your browser";
+    } else {
+      status.textContent = "Locating…";
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }
+  
+  document.querySelector("#find-me").addEventListener("click", geoFindMe);
 
-const button = document.querySelector("login-page-navlink1 button")
-button.addEventListener("click", () => {
-    alert("Zalogowano")
-})
-
-
-
+    const button = document.getElementById("register_btn");
+    button.addEventListener("click", () => {
+      Notification.requestPermission().then((result) => {
+        if (result === "granted") {
+            new Notification("Udało ci sie zarejestrować! Zaloguj się!");
+        }
+      });
+    });
 
 checkPermission()
 registerSW()
-requestNotificationPermission()
